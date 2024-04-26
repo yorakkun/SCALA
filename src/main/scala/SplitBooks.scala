30ecc7883387225118fa480ececdc568abec6b28
Juan Lenis <juan-camilo.lenis@etu.univ-lyon1.fr>
2024-04-26 12:59:19 +0200

    aguacate

--- src/main/scala/SplitBooks.scala
+++ src/main/scala/SplitBooks.scala
@@ -20,6 +20,9 @@ object SimpleApp extends App {
   // Group by 'bookId' and aggregate contents
   val books = contentData.groupBy($"bookId").agg(collect_list($"clean_value").as("content"))
 
+  // DF pour conter les lignes par livres (elles seront supprimées dans l'étape suivante)
+  val analysisArray = cleanedData.groupBy($"bookId").agg(count("*").as("lines"))
+
   // Convert the aggregated content into a single string per book and assign IDs
   val finalBooks = books.withColumn("id", monotonically_increasing_id())
                         .withColumn("content", concat_ws(" ", $"content"))
@@ -30,6 +33,43 @@ object SimpleApp extends App {
 
   // Convert to Dataset of a case class if needed for better type safety
   finalBooks.as[(Long, String)].show(true)
+
+  // Lignes pour le calcul de la moyenne et mediane TOTALE
+  val nbLignes = cleanedData.count()
+
+  // Calcul des mots PAR LIVRE
+  val finalBooksMots = finalBooks.withColumn("mots", size(split(col("content"), "\\s+")))
+
+  val joinedDF = finalBooksMots.join(analysisArray, finalBooksMots("id") === analysisArray("bookId"), "left")
+  joinedDF.show()
+
+  val finalAnalysis = joinedDF.withColumn("Mots/Ligne", $"mots"/$"lines")
+                      .select("id", "mots", "lines", "Mots/Ligne")
+  finalAnalysis.show()
+  // On separe les lignes en mots (String => Array[String])
+  // (Vu que logData est déjà un DataSet on peux appliquer un map directement
+  // on peut donc separer les lignes en mots en utilisant selectExpr)
+  val motsLigne = finalBooks.selectExpr("split(content, ' ') as mots")
+
+  // Puis on explode les Arrays des mots pour avoir un Array avec tous les mots du texte
+  // et on fais juste le count de cet array.
+  val allMots = motsLigne.selectExpr("explode(mots) as mot")
+  //allMots.show()
+  val nbMots = allMots.count()
+
+  // Calcul de la moyenne de mots par ligne
+  val moyMots = nbMots/nbLignes
+
+  // Calcul de la mediane de mots par ligne
+  val aName = allMots.selectExpr("length(mot) as lg")
+  val lignes = aName.orderBy("lg").select("lg").collect()
+  val medMots = lignes((nbMots/2).toInt)
+  
+  //println(s"Il y a $nbLignes Lignes dans le texte")
+  println(s"Il y a $nbMots Mots dans le texte")
+  println(s"Il y a $nbLignes Lignes dans le texte")
+  println(s"Il y a en moyenne $moyMots Mots par Ligne dans le texte")
+  println(s"Il y a en mediane $medMots Mots par Ligne dans le texte")
 
   spark.stop()
 }
