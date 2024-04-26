c6a3e860bcdde9f81b0a6b4fc59e129a4771bb81
Asrielbot <49340417+Asrielbot@users.noreply.github.com>
2024-04-26 12:09:27 +0200

    Clean data and split working

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,24 +1,23 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.Window
+import Nettoyage.ClearData
 
 object SimpleApp extends App {
   val spark = SparkSession.builder.appName("Simple Application").master("local[*]").getOrCreate()
   import spark.implicits._
 
-  // Load the text file into a DataFrame
-  val logFile = "./data/testing.txt" // Adjust the path as needed
-  val textData = spark.read.text(logFile).as[String]
+  val cleanedData = ClearData(spark)
 
   // Identify lines containing "ISBN" as new book starts and assign IDs
-  val withBookStarts = textData.withColumn("isNewBook", lower($"value").contains("isbn"))
+  val withBookStarts = cleanedData.withColumn("isNewBook", lower($"value").contains("isbn"))
   val bookIds = withBookStarts.withColumn("bookId", sum(when($"isNewBook", 1).otherwise(0)).over(Window.orderBy(monotonically_increasing_id())))
 
   // Filter out the ISBN lines if you want only content, adjust as needed
-  val cleanedData = bookIds.filter(!$"isNewBook")
+  val contentData = bookIds.filter(!$"isNewBook")
 
   // Group by 'bookId' and aggregate contents
-  val books = cleanedData.groupBy($"bookId").agg(collect_list($"value").as("content"))
+  val books = contentData.groupBy($"bookId").agg(collect_list($"value").as("content"))
 
   // Convert the aggregated content into a single string per book and assign IDs
   val finalBooks = books.withColumn("id", monotonically_increasing_id())
